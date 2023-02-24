adcbed6eac6a1acd4e79faddb37fbd0b3b4365eb
ern0 <ern0@linkbroker.hu>
2023-02-24 19:33:20 +0100

    Split ClientManager

---
+++ app/server/src/client_manager.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+use simple_websockets::{ Event, EventHub };
+
+use crate::client::Client;
+
+pub struct ClientManager {
+	event_hub: EventHub,
+	clients: HashMap<u64, Client>,
+}
+
+impl ClientManager {
+
+	pub fn new(event_hub: EventHub) -> Self {
+		return Self {
+			event_hub: event_hub,
+			clients: HashMap::new(),
+		}
+	}
+
+	pub fn run(mut self: ClientManager) {
+
+    loop { 
+
+        match self.event_hub.poll_event() {
+
+            Event::Connect(client_id, responder) => {
+                
+                println!("A client connected with id #{}", client_id);
+
+                let client = Client { 
+                    id: client_id, 
+                    responder: responder,
+                };
+                self.clients.insert(client.id, client);
+
+            },
+
+            Event::Disconnect(client_id) => {
+
+                println!("Client #{} disconnected.", client_id);
+                self.clients.remove(&client_id);
+                
+            },
+
+            Event::Message(client_id, message) => {
+
+                println!("Received a message from client #{}: {:?}", client_id, message);
+
+                let client = self.clients.get(&client_id).unwrap();
+                client.process_incoming_message(message);
+
+            },
+
+        }
+
+    }
+
+	}
+
+}
--- app/server/src/main.rs
+++ app/server/src/main.rs
@@ -1,51 +1,14 @@
-use simple_websockets;
-use std::collections::HashMap;
-
+mod client_manager;
 mod client;
-use client::Client;
+
+use simple_websockets;
+use client_manager::ClientManager;
 
 pub fn main() {
 
     let event_hub = simple_websockets::launch(8080)
         .expect("failed to listen on port 8080");
 
-    let mut clients: HashMap<u64, Client> = HashMap::new();
-
-    loop {
-
-        match event_hub.poll_event() {
-
-            simple_websockets::Event::Connect(client_id, responder) => {
-                
-                println!("A client connected with id #{}", client_id);
-
-                let client = Client { 
-                    id: client_id, 
-                    responder: responder,
-                };
-                clients.insert(client.id, client);
-
-            },
-
-            simple_websockets::Event::Disconnect(client_id) => {
-
-                println!("Client #{} disconnected.", client_id);
-
-                clients.remove(&client_id);
-                
-            },
-
-            simple_websockets::Event::Message(client_id, message) => {
-
-                println!("Received a message from client #{}: {:?}", client_id, message);
-
-                let client = clients.get(&client_id).unwrap();
-                let response = client_id.to_string();
-                client.responder.send(simple_websockets::Message::Text(response));
-            },
-
-        }
-
-    }
-
+    let client_manager = ClientManager::new(event_hub);
+    client_manager.run();
 }
