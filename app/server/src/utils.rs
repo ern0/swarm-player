800b3c3cbf493c12fb71d7e7cea58f957948c8c5
ern0 <ern0@linkbroker.hu>
2023-03-03 18:47:32 +0100

    Added packet tests

--- app/server/src/utils.rs
+++ app/server/src/utils.rs
@@ -1,5 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
+pub const UNDEF: i64 = i64::MAX;
+
 pub fn json_add_key(result: &mut String, value: &str) {
     json_add_quoted(result, value);
     result.push_str(":");
