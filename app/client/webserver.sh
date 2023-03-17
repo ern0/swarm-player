ea7b8bd6a5bd4d45ead9c15c7e7ad7ee73726872
ern0 <ern0@linkbroker.hu>
2023-03-17 09:34:10 +0100

    Fixed Linux devops stuff: nginx logging, nginx start/stop

--- app/client/webserver.sh
+++ app/client/webserver.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 clear
 
-nginx -s quit 2> /dev/null
+if [ `uname -s` = "Linux" ]; then
+	LINUX_SUDO=sudo
+else
+	LINUX_SUDO=
+fi
+$LINUX_SUDO nginx -s quit 2> /dev/null
+
 set -e
 
 export host=`ifconfig | grep 192 | cut -d' ' -f2`
+if [ -z $host ]; then
+	export host=`ifconfig | grep "inet 10" | cut -d't' -f2 | cut -d' ' -f2`
+fi
 export pwd=`pwd`
 export cert_crt=`pwd`/cert.crt
 export cert_key=`pwd`/cert.key
 
 if [ ! -f "$cert_crt" ]; then
+
+	if [ -z $host ]; then
+		echo variable \$host is not set
+		exit
+	fi
+
 	openssl req \
 		-subj "/CN=$host" \
 		-nodes -x509 -newkey rsa:4096 \
@@ -21,7 +36,8 @@ fi
 export http_upgrade="\$http_upgrade"
 envsubst <nginx.conf >/tmp/nginx.conf
 
-nginx -c /tmp/nginx.conf
+
+$LINUX_SUDO nginx -c /tmp/nginx.conf
 ps -e | grep nginx | grep -v grep
 read
-nginx -s quit
+$LINUX_SUDO nginx -s quit
