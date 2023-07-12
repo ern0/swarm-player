5a96ac9a67cc85fcd1c61e82e2054c7dd49e5af6
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    Response commented out, server does not work

--- app/server/src/client.rs
+++ app/server/src/client.rs
@@ -1,8 +1,10 @@
-use simple_websockets::Message;
+#![allow(unused)]
+
+use simple_websockets::{ Message, Responder };
 
 pub struct Client {
 	pub id: u64,
-	pub responder: simple_websockets::Responder,
+	//pub responder: Responder,
 }
 
 impl Client {
@@ -10,7 +12,7 @@ impl Client {
 	pub fn process_incoming_message(self: &Client, _: simple_websockets::Message) {		
 
         let response = self.id.to_string();
-		self.responder.send(Message::Text(response));
+		//self.responder.send(Message::Text(response));
 
 	}
 
