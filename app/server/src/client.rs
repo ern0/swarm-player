2ccc362fb567831f6827da488e06a74ad451c94a
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    working on process_incoming()

--- app/server/src/client.rs
+++ app/server/src/client.rs
@@ -17,24 +17,25 @@ impl Client {
 
             let parsed: JsonValue = text.parse().unwrap();
             let root_object: &HashMap<_, _> = parsed.get().unwrap();
-
             let message_type = self.parse_message_type(root_object);
-
             println!("---------[{}]", message_type);
-        }
 
+            if message_type == "req.clk" {
+                //let arg = self.parse_message_data_int(root_object, 1);
+                //process_request_clock(arg);
+            }
+        }
 
 	}
 
     fn parse_message_type(&self, root_object: &HashMap<String, JsonValue>) -> String {
 
-        //let type_value: JsonValue = root_object.get(&String::from("type")).unwrap();            
-        //let mut message_type: String = String::from("n/a");
-        //if let JsonValue::String(string) = type_value {
-        //    message_type = string.to_string();
-        // }
+        let type_value: &JsonValue = root_object.get(&String::from("type")).unwrap();
+        if let JsonValue::String(string) = type_value {
+            return string.to_string();
+        }
 
-        return String::from("lof");
+        return String::from("n.a.");
     }
 
 }
