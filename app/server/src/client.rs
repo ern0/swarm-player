06d201b3f8b90cd1618d9b37c29aeed4339c71de
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    Split ClientManager

--- app/server/src/client.rs
+++ app/server/src/client.rs
@@ -1,7 +1,17 @@
-use simple_websockets;
+use simple_websockets::Message;
 
 pub struct Client {
 	pub id: u64,
 	pub responder: simple_websockets::Responder,
 }
 
+impl Client {
+
+	pub fn process_incoming_message(self: &Client, _: simple_websockets::Message) {		
+
+        let response = self.id.to_string();
+		self.responder.send(Message::Text(response));
+
+	}
+
+}
