edc32950dafef3cd3e4932f0b447e72f3d35aa89
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    Refactor, reformat

--- app/server/src/client_manager.rs
+++ app/server/src/client_manager.rs
@@ -1,44 +1,44 @@
 #![allow(unused)]
 
 use std::collections::HashMap;
-use std::sync::{ Arc, Mutex };
-use simple_websockets::{ Event, EventHub, Responder, Message };
-
+use std::sync::{Arc, Mutex};
+use simple_websockets::{Event, EventHub, Message, Responder};
+use crate::utils::{json_add_key, json_add_quoted, now};
 use crate::client::Client;
 
 pub struct ClientManager {
-	event_hub: EventHub,
-	clients: Mutex<HashMap<u64, Client>>,
+    event_hub: EventHub,
+    clients: Mutex<HashMap<u64, Client>>,
 }
 
 impl ClientManager {
 
-	pub fn new(event_hub: EventHub) -> Self {
-		return ClientManager {
-			event_hub: event_hub,
-			clients: Mutex::new(HashMap::new()),
-		}
-	}
+    pub fn new(event_hub: EventHub) -> Self {
 
-	pub fn run(&self) {
+        return ClientManager {
+            event_hub: event_hub,
+            clients: Mutex::new(HashMap::new()),
+        };
+    }
 
-        loop { 
+    pub fn run(&self) {
+
+        loop {
             match self.event_hub.poll_event() {
-
                 Event::Connect(client_id, responder) => {
-                    self.on_connect(responder, client_id);                    
-                },
+                    self.on_connect(responder, client_id);
+                }
 
                 Event::Disconnect(client_id) => {
-                    self.on_disconnect(client_id);                
-                },
+                    self.on_disconnect(client_id);
+                }
 
                 Event::Message(client_id, message) => {
                     self.on_message(client_id, message);
-                },
-
+                }
             }
         }
+
     }
 
     fn on_connect(&self, responder: Responder, client_id: u64) {
@@ -47,7 +47,6 @@ impl ClientManager {
 
         let client = Client::new(client_id, responder);
         self.clients.lock().unwrap().insert(client.id, client);
-
     }
 
     fn on_disconnect(&self, client_id: u64) {
@@ -55,20 +54,18 @@ impl ClientManager {
         ////println!("disconnected, id={}", client_id);
 
         self.clients.lock().unwrap().remove(&client_id);
-
     }
 
     fn on_message(&self, client_id: u64, message: Message) {
 
         if let Message::Text(text) = message {
-            
+
             ////println!("received, client={} message={}", client_id, text);
 
             let mut hash_map = self.clients.lock().unwrap();
             let client: &mut Client = hash_map.get_mut(&client_id).unwrap();
             client.process_incoming_message(text);
         }
-
     }
 
     pub fn broadcast(&self) {
@@ -76,10 +73,12 @@ impl ClientManager {
         let hash_map = self.clients.lock().unwrap();
         let size = hash_map.len();
 
+        //TODO: send JSON
+
         ////println!("Sending broadcast: {}", size);
 
         for (_id, client) in hash_map.iter() {
-       		client.responder.send(Message::Text(size.to_string()));
+            client.responder.send(Message::Text(size.to_string()));
         }
     }
 
--- app/server/src/main.rs
+++ app/server/src/main.rs
@@ -1,16 +1,16 @@
-mod client_manager;
 mod client;
+mod client_manager;
+mod utils;
 
-use simple_websockets;
 use client_manager::ClientManager;
-use std::time::{ Duration };
-use std::thread::{ sleep, spawn };
+use simple_websockets;
 use std::sync::Arc;
+use std::thread::{sleep, spawn};
+use std::time::Duration;
 
 pub fn main() {
-
-    let event_hub = simple_websockets::launch(8080)
-        .expect("failed to listen on port 8080");
+    
+    let event_hub = simple_websockets::launch(8080).expect("failed to listen on port 8080");
     println!("server is up");
 
     let client_manager = ClientManager::new(event_hub);
@@ -23,5 +23,4 @@ pub fn main() {
         cm2.broadcast();
         sleep(Duration::from_secs(1));
     }
-
 }
