f69209e71b56c7f47300e4f27e04dfb82f276995
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    Refined heartbeat and clock sync timing. Heartbeat now sends some stat.

--- app/client/client.js
+++ app/client/client.js
@@ -1,4 +1,6 @@
-WEBSOCKET_PORT = 8080
+WEBSOCKET_PORT = 8080;
+HEARTBEAT_TIMING_S = [0.3, 10];
+CLOCK_SYNC_TIMING_S = [ 0, 0.5, 2, 5, 30 ];
 
 document.addEventListener("DOMContentLoaded", main);
 
@@ -8,6 +10,7 @@ function main() {
 	init_url_options();
 	clock_sync_reset();
 	init_bind_buttons();
+	reset_stat();
 
 	app.intent = "offline";
 	page("welcome");
@@ -42,6 +45,7 @@ function init_url_connection(url) {
 	app.server_url = protocol + "//";
 	app.server_url += url.hostname;
 	app.server_url += ":" + WEBSOCKET_PORT;
+
 }
 
 function init_url_option_skew(url) {
@@ -83,6 +87,14 @@ function init_bind_buttons() {
 	elm("cmd_blue").onclick = handle_button_cmd;
 	elm("cmd_yellow").onclick = handle_button_cmd;
 	elm("cmd_gray").onclick = handle_button_cmd;
+
+}
+
+function reset_stat() {
+
+	app.stat_count = 0;
+	app.stat_min_delay = 0;
+	app.stat_max_delay = 0;
 
 }
 
@@ -158,7 +170,7 @@ function handle_socket_open(event) {
 
 	page("op");
 
-	schedule_heartbeat(500);
+	schedule_heartbeat(HEARTBEAT_TIMING_S[0]);
 	clock_sync_reset();
 	clock_sync_start();	
 
@@ -176,6 +188,15 @@ function handle_socket_message(event) {
 	var now = get_clock();
 	var action = packet.stamp;
 	var delay = action - now;
+
+	app.stat_count += 1;
+	if (app.stat_min_delay == 0 || app.stat_min_delay < delay) {
+		app.stat_min_delay = delay;
+	}
+	if (app.stat_max_delay == 0 || app.stat_max_delay > delay) {
+		app.stat_max_delay = delay;
+	}
+
 	if (delay < 1) delay = 1;
 
 	setTimeout(function() {
@@ -239,17 +260,26 @@ function send(signature, args) {
 	data = JSON.stringify(packet);
 	app.websocket.send(data);
 
-	schedule_heartbeat(5000);  //TODO: change to prod value
+	schedule_heartbeat(HEARTBEAT_TIMING_S[1]);
 }
 
 function send_heartbeat() {
-	send("HEARTBEAT", [get_clock(), app.clock_skew]);
+
+	send("HEARTBEAT", [
+		app.clock_skew,
+		app.stat_count,
+		app.stat_min_delay,
+		app.stat_max_delay
+	]);
+
+	reset_stat();
+
 }
 
-function schedule_heartbeat(timeout) {
+function schedule_heartbeat(timeout_s) {
 
 	stop_heartbeat();	
-	app.heartbeat = setTimeout(send_heartbeat, timeout);
+	app.heartbeat = setTimeout(send_heartbeat, timeout_s * 1000);
 
 }
 
@@ -305,20 +335,22 @@ function clock_sync_calc_skew(clock_ref) {
 	var skew = Math.round(estimation - clock_ref);
 
 	var change = Math.abs(app.clock_skew - skew);
-	if (change > 50) app.clock_skew = skew;
+	if (change > 30) app.clock_skew = skew;
 
 }
 
 function clock_sync_reschedule() {
 
-	var sleep_duration_s = 2;
-	if (app.clock_sync_round > 0) sleep_duration_s = 5;
-	if (app.clock_sync_round > 2) sleep_duration_s = 15;
-	if (app.clock_sync_round > 5) sleep_duration_s = 30;
+	if (app.clock_sync_round >= CLOCK_SYNC_TIMING_S.length) {
+		var sleep_duration_s = CLOCK_SYNC_TIMING_S[CLOCK_SYNC_TIMING_S.length - 1];
+	} else {
+		var sleep_duration_s = CLOCK_SYNC_TIMING_S[app.clock_sync_round];
+	}
 	app.clock_sync_round += 1;
 
 	var sleep_duration_ms = sleep_duration_s * 1000;
-	sleep_duration_ms += Math.random() * 5000;
+	sleep_duration_ms += Math.random() * (sleep_duration_ms / 10);
 
 	setTimeout(clock_sync_start, sleep_duration_ms);
+	
 }
