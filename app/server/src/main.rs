a93b4928fcb8be677830a322fc66681913df23ae
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    Need to solve: run_broadcast()

--- app/server/src/main.rs
+++ app/server/src/main.rs
@@ -18,21 +18,21 @@ pub fn main() {
     let event_hub = simple_websockets::launch(8080).expect("failed to listen on port 8080");
     println!("server is up");
 
-    let (sender, receiver,) = mpsc::channel();
+    //let (sender, receiver,) = mpsc::channel();
 
-    let client_manager = ClientManager::new());
+    let client_manager = ClientManager::new();
     let cm0 = Arc::new(client_manager);
     let cm1 = cm0.clone();
     let cm2 = cm1.clone();
 
     spawn(move || cm1.run_event_hub(&event_hub));
-    spawn(move || cm2.run_broadcast(&receiver));
+    spawn(move || cm2.run_broadcast());
 
     let mut counter = 0;
     loop {        
-        let mut packet = Packet::new_simple_num("DISPLAY", counter);    
+        let mut packet = Packet::new_simple_num("DISPLAY", counter);
         packet.set_num(0, counter);
-        sender.send(packet).unwrap();
+        cm0.broadcast(&packet);
         counter += 1;
 
         sleep(Duration::from_secs(1));
