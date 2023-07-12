ddc643a9aa2516821d43263680835c7f584dcf8d
ern0 <ern0@linkbroker.hu>
2023-07-12 13:14:50 +0200

    Changed webserver to nginx, need HTTPS/WSS proxy

--- app/client/webserver.sh
+++ app/client/webserver.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+clear
 
-host=`ifconfig | grep 192 | cut -d' ' -f2`
+nginx -s quit 2> /dev/null
+set -e
 
-caddy file-server --domain $host &
-caddy file-server --listen :8000 &
+export host=`ifconfig | grep 192 | cut -d' ' -f2`
+export pwd=`pwd`
+export cert_crt=`pwd`/cert.crt
+export cert_key=`pwd`/cert.key
+
+if [ ! -f "$cert_crt" ]; then
+	openssl req \
+		-subj "/CN=$host" \
+		-nodes -x509 -newkey rsa:4096 \
+		-keyout $cert_key \
+		-out $cert_crt \
+		-sha256 -days 365
+fi
+
+envsubst <nginx.conf >/tmp/nginx.conf
+
+nginx -c /tmp/nginx.conf
+ps -e | grep nginx | grep -v grep
 read
-pkill caddy
+nginx -s quit
