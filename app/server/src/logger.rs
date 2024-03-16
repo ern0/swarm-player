c9e43163ed5ff60e08f6c45311bd251dc7b11190
ern0 <ern0@linkbroker.hu>
2024-03-16 22:39:32 +0100

    Reformat

--- app/server/src/logger.rs
+++ app/server/src/logger.rs
@@ -3,30 +3,26 @@ use simple_websockets::*;
 use crate::utils::{Port};
 
 pub struct Logger {
-	debug_mode: bool,
+    debug_mode: bool,
 }
 
 impl Logger {
+    pub fn new(debug_mode: bool) -> Self {
+        Self {
+            debug_mode
+        }
+    }
 
-	pub fn new(debug_mode: bool) -> Self {
-		Self {
-				debug_mode,
-		}
-	}
+    pub fn log_webserver_okay(&self, port: Port) {
+        println!("[mgr]: server is up, listening on port {}",
+            port
+        );
+    }
 
-	pub fn log_webserver_okay(&self, port: Port) {
-			println!(
-					"[mgr]: server is up, listening on port {}",
-					port,
-			);
-	}
-
-	pub fn log_webserver_fail(&self, port: Port, error: &simple_websockets::Error) {
-		println!(
-				"[mgr]: failed to launch server on port {}: {:?}",
-				port,
-				error,
-		);
-	}
-
+    pub fn log_webserver_fail(&self, port: Port, error: &simple_websockets::Error) {
+        println!(
+            "[mgr]: failed to launch server on port {}: {:?}",
+            port, error,
+        );
+    }
 }
